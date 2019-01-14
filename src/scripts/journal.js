35e2bfeed7cc09231972f3c3fd211a477b48afdd
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-01-14 13:55:22 -0600

    moved js file to proper directory

---
+++ src/scripts/journal.js
@@ -0,0 +1,14 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const objectsJournalEntry = {
+    dayOfWeek: "monday",
+    concepts: "functions and objects",
+    mood: "chill",
+    levelOfComprehension: "need practice",     
+}
+
+let dailyEntries = []
+dailyEntries.push(objectsJournalEntry);
+console.log(dailyEntries)
