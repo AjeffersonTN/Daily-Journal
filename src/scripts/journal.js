97fc599fbae55574fe5eab5ce953422050df0e13
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-01-31 13:14:58 -0600

    journal entry 3 completed

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,33 +1,63 @@
-/*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
-*/
-const objectsJournalEntry = {
-    dayOfWeek: "Monday",
-    concepts: "JavaScript Functions",
-    mood: "Chill",
-    levelOfComprehension: "Need Practice",     
-}
-const objectsJournalEntry1 = {
-    dayOfWeek: "Tuesday",
-    concepts: "Querying the DOM",
-    mood: "Happy",
-    levelOfComprehension: "Need Practice",     
-}
-const objectsJournalEntry2 = {
-    dayOfWeek: "Wednesday",
-    concepts: "JavaScript Objects",
-    mood: "Chill",
-    levelOfComprehension: "Need Practice",     
-}
-const objectsJournalEntry3 = {
-    dayOfWeek: "Thursday",
-    concepts: "Git Hub Workflow",
-    mood: "Sad",
-    levelOfComprehension: "Understand",     
-}
 
+let journalEntries  = [
+    {
+        date: "01/12/18",
+        concepts: "JavaScript Functions",
+        entry: "Need Practice",
+        mood: "Chill",
+    },
 
-let dailyEntries = []
-dailyEntries.push(objectsJournalEntry, objectsJournalEntry1, objectsJournalEntry2, objectsJournalEntry3);
-console.log(dailyEntries)
+    {
+        date: "01/12/18",
+        concepts: "Querying the DOM",
+        entry: "Need Practice",
+        mood: "Happy",
+    },
+
+    {
+        date: "01/12/18",
+        concepts: "JavaScript Objects",
+        entry: "Need Practice",
+        mood: "Chill",
+    },
+
+    {
+        date: "01/12/18",
+        concepts: "Git Hub Workflow",
+        entry: "Need Practice",
+        mood: "Sad",
+    }
+
+]
+// journalEntries.push();
+// console.log(journalEntries )
+let putOnDOM = document.querySelector(".entryLog")
+
+//Function for HTML representation
+let makeJournalEntryComponent = (currentOne) => {
+
+    return `
+    <div class="entryLog">
+    <p><strong>${currentOne.concepts}</strong></p>
+    <p>Date: ${currentOne.date}</p>
+    <p>Entry: ${currentOne.entry}</p>
+    <p>Mood: ${currentOne.mood}</p>
+    </div>
+    `
+}
+//function to add to DOM
+let displayOnPage = (putItOn) => {
+    putOnDOM.innerHTML += putItOn;
+}
+//function to iterate over the array and invoke makeJournalEntryComponent
+const renderJournalEntries = (entries) => {
+    for (let i = 0; i < entries.length; i++) {
+        let currentOne = entries[i];
+        console.log(currentOne)
+       let returnFromMAGA = makeJournalEntryComponent(currentOne) //had to capture the input by setting equal to a variable in order to utlize
+       displayOnPage(returnFromMAGA)
+
+    }
+}
+renderJournalEntries(journalEntries)
+
