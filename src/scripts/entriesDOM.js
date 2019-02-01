f9ec99836f396363ad4efac504a85a6e0f6acdec
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-02-01 11:19:58 -0600

    journal 5 challenge changed to objects but didn't used Object.create

--- src/scripts/entriesDOM.js
+++ src/scripts/entriesDOM.js
@@ -1,6 +1,12 @@
 const putOnDOM = document.querySelector(".entryLog")
-
-//function to add-to/modify the DOM
-let displayOnPage = (putItOn) => {
+//change to method on an object
+let displayOnPageObject = { 
+ displayOnPage(putItOn){
     putOnDOM.innerHTML += putItOn;
+    }
 }
+//function to add-to/modify the DOM
+// let displayOnPage = (putItOn) => {
+//     putOnDOM.innerHTML += putItOn;
+// }
+// const displayOnPageObject = Object.create(displayOnPage); COME BACK LATER
--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -2,12 +2,29 @@
 function loopTheEntries (entries){
     for (let i = 0; i < entries.length; i++) {
     let currentOne = entries[i];
-    let returnFromMAGA = makeJournalEntryComponent(currentOne) //had to capture the input by setting equal to a variable in order to utlize
-   displayOnPage(returnFromMAGA)
+    let returnFromMAGA = makeJournalEntryComponentObject.makeJournalEntryComponent(currentOne) //had to capture the input by setting equal to a variable in order to utlize
+    displayOnPageObject.displayOnPage(returnFromMAGA)
+    //used prior to making the displayOnPageObject
+    // displayOnPage(returnFromMAGA)
+
 
 }}
 
 //Function for creating the HTML representation
+makeJournalEntryComponentObject ={
+    makeJournalEntryComponent(currentOne){
+    return `
+    <div class="entryLog">
+    <p><strong>${currentOne.concept}</strong></p>
+    <p>Date: ${currentOne.date}</p>
+    <p>Entry: ${currentOne.entry}</p>
+    <p>Mood: ${currentOne.mood}</p>
+    </div>
+    `
+    }
+}
+/*
+//Function for creating the HTML representation used prior to above
 let makeJournalEntryComponent = (currentOne) => {
 
     return `
@@ -19,3 +36,4 @@ let makeJournalEntryComponent = (currentOne) => {
     </div>
     `
 }
+*/
