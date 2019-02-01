b156d0b1c9103b96ee8e9eb25caca8a9acd9bc74
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-02-01 09:22:56 -0600

    daily journal 4 complete- need to add actual journal entries

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,44 +1,27 @@
+fetch(`http://localhost:3000/entries`)
+    .then(response => response.json())
+    .then(entries => {
+        //function to iterate over the array and invoke makeJournalEntryComponent
+    for (let i = 0; i < entries.length; i++) {
+        let currentOne = entries[i];
 
-let journalEntries  = [
-    {
-        date: "01/12/18",
-        concepts: "JavaScript Functions",
-        entry: "Need Practice",
-        mood: "Chill",
-    },
+        let returnFromMAGA = makeJournalEntryComponent(currentOne) //had to capture the input by setting equal to a variable in order to utlize
+       displayOnPage(returnFromMAGA)
 
-    {
-        date: "01/12/18",
-        concepts: "Querying the DOM",
-        entry: "Need Practice",
-        mood: "Happy",
-    },
-
-    {
-        date: "01/12/18",
-        concepts: "JavaScript Objects",
-        entry: "Need Practice",
-        mood: "Chill",
-    },
-
-    {
-        date: "01/12/18",
-        concepts: "Git Hub Workflow",
-        entry: "Need Practice",
-        mood: "Sad",
     }
+}
 
-]
+    )
 // journalEntries.push();
 // console.log(journalEntries )
-let putOnDOM = document.querySelector(".entryLog")
+const putOnDOM = document.querySelector(".entryLog")
 
 //Function for HTML representation
 let makeJournalEntryComponent = (currentOne) => {
 
     return `
     <div class="entryLog">
-    <p><strong>${currentOne.concepts}</strong></p>
+    <p><strong>${currentOne.concept}</strong></p>
     <p>Date: ${currentOne.date}</p>
     <p>Entry: ${currentOne.entry}</p>
     <p>Mood: ${currentOne.mood}</p>
@@ -49,15 +32,3 @@ let makeJournalEntryComponent = (currentOne) => {
 let displayOnPage = (putItOn) => {
     putOnDOM.innerHTML += putItOn;
 }
-//function to iterate over the array and invoke makeJournalEntryComponent
-const renderJournalEntries = (entries) => {
-    for (let i = 0; i < entries.length; i++) {
-        let currentOne = entries[i];
-        console.log(currentOne)
-       let returnFromMAGA = makeJournalEntryComponent(currentOne) //had to capture the input by setting equal to a variable in order to utlize
-       displayOnPage(returnFromMAGA)
-
-    }
-}
-renderJournalEntries(journalEntries)
-
